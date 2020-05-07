d9187f97af5cd825bcbf298a7f052c5a32c92028
Pedro Lemos <pedroemlemos@gmail.com>
2020-05-07 01:11:39 -0300

    Base Spring Boot application

---
+++ src/main/java/com/voting/service/VotingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.voting.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voting/service/VotingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voting.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
