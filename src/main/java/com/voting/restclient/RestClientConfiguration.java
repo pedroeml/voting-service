2ae59e0b8322c83c5c716a83bfa47a6d8dd0b38b
Pedro Lemos <pedroemlemos@gmail.com>
2020-05-07 20:45:31 -0300

    RestClientConfiguration and new dependency

---
+++ src/main/java/com/voting/restclient/RestClientConfiguration.java
@@ -0,0 +1,29 @@
+package com.voting.restclient;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class RestClientConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        final List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+        messageConverters.add(converter);
+
+        return builder
+            .messageConverters(messageConverters)
+            .build();
+    }
+}
